78872041f1b1cc44ccd1988525570ad7207fe174
Christoph Minz <christoph.minz@gmail.com>
2022-01-10 23:26:34 +0100

    Work in progress: generating a permutation for a causet to draw a Hasse diagram. Started work on a search function for closed fences between a past and a future antichain.

--- causetpermutations.py
+++ causetpermutations.py
@@ -170,8 +170,8 @@ def layeredPermutations(C: Causet, eventSet: Set[CausetEvent] = None) -> \
                 layers_perm[k] = next(layer_iterators[k])
 
 
-def permuted(C: Causet, eventSet: Set[CausetEvent] = None,
-             maxIterations: int = 100000) -> \
+def permuted_old(C: Causet, eventSet: Set[CausetEvent] = None,
+                 maxIterations: int = 100000) -> \
         Tuple[List[CausetEvent], List[int]]:
     '''
     Searches for a permutation of integers from 1 to len(eventSet) + 1 
@@ -288,3 +288,247 @@ def permuted(C: Causet, eventSet: Set[CausetEvent] = None,
             else:
                 continue
     return (L, P)
+
+# new development (started December 2021) below:
+
+
+def findClosedFences(PStart: List[CausetEvent],
+                     FStart: List[CausetEvent],
+                     PAchain: Set[CausetEvent],
+                     FAchain: Set[CausetEvent]) -> \
+        List[Tuple[List[CausetEvent], List[CausetEvent], int]]:
+    '''
+    Searches for causal, closed Fences between events of the 
+    past antichain set `PAchain` and the future antichain set 
+    `FAchain`. All closed Fences have to begin with the events 
+    `PStart` in the past and `FStart` in the future where:
+     `FStart`:  0  1  2         0  1  2  ...
+                |\ |\ |\     or | /| /| /
+                | \| \| \       |/ |/ |/
+     `PStart`:  0  1  2  ...    0  1  2
+    At least one of the lists has to be non-empty and the other 
+    list has to have the same number of elements or exactly one 
+    more or less, so that possible starts are:
+     `FStart`:  0         0    0       0  1    0  1    0  1
+                  or   or | or |\   or | /  or |\ | or | /| ...
+                          |    | \     |/      | \|    |/ |
+     `PStart`:       0    0    0  1    0       0  1    0  1
+
+    The function returns a list of closed Fences each including 
+    the events `PStart` and `FStart` and any events from the sets 
+    `PAchain` and `FAchain` at most once among all returned values.
+    Examples of closed Fences are:
+     orientation:  -1/1         -1             1                1
+     fence future: 0  1    0  2  1    0  1  3  2    0  1  4  2  3
+                   |\/| or |\/ \/| or |\/ \/ \/| or |\/ \/ \/ \/| ...
+                   |/\|    |/\ /\|    |/\ /\ /\|    |/\ /\ /\ /\|
+     fence past:   0  1    0  1  2    0  3  1  2    0  4  1  3  2
+     return: list of (fence past, fence future, orientation)
+    '''
+    # Check if the given fence (start) is valid:
+    PStartSet: Set[CausetEvent] = set(PStart)
+    FStartSet: Set[CausetEvent] = set(FStart)
+    PStartCount: int = len(PStartSet)
+    FStartCount: int = len(FStartSet)
+    isStartCountsEqual: bool = PStartCount == FStartCount
+    isValidStart: bool = (PStartCount > 0 or FStartCount > 0) and \
+        PStartCount == len(PStart) and \
+        FStartCount == len(FStart) and \
+        abs(PStartCount - FStartCount) <= 1 and \
+        PStartSet.issubset(PAchain) and \
+        FStartSet.issubset(FAchain)
+    isStartShapeN: bool = False  # Does it start in a Lambda shape?
+    isStartShapeV: bool = False  # Does it start in a V shape?
+    isClosedFenceN: bool = False
+    isClosedFenceV: bool = False
+    TerminalSet: Set[CausetEvent]
+    if PStartCount >= FStartCount:
+        isStartShapeN = isValidStart and \
+            all(FStart[i].Past & PStartSet ==
+                {PStart[i], PStart[i + 1]}
+                for i in range(PStartCount - 1))
+        if isStartCountsEqual:
+            TerminalSet = FStart[-1].Past & PStartSet
+            isClosedFenceN = (PStartCount > 1) and \
+                (TerminalSet == {PStart[-1], PStart[0]})
+            isStartShapeN = isStartShapeN and \
+                (isClosedFenceN or (TerminalSet == {PStart[-1]}))
+    if FStartCount >= PStartCount:
+        isStartShapeV = isValidStart and \
+            all(PStart[i].Future & FStartSet ==
+                {FStart[i], FStart[i + 1]}
+                for i in range(FStartCount - 1))
+        if isStartCountsEqual:
+            TerminalSet = PStart[-1].Future & FStartSet
+            isClosedFenceV = (FStartCount > 1) and \
+                (TerminalSet == {FStart[-1], FStart[0]})
+            isStartShapeV = isStartShapeV and \
+                (isClosedFenceV or (TerminalSet == {FStart[-1]}))
+    if (isStartShapeN and isClosedFenceN) or \
+            (isStartShapeV and isClosedFenceV):
+        return [(PStart, FStart, -1 if isStartShapeN else 1)]
+    elif not isStartShapeN and not isStartShapeV:
+        return []
+#         raise Exception('The specified lists `PStart` and `FStart` ' +
+#                         'do not describe the start of a fence.')
+    # Search and complete the fence start to closed fences:
+    PAchain = PAchain - PStartSet
+    FAchain = FAchain - FStartSet
+    Fences: List[Tuple[List[CausetEvent], List[CausetEvent], int]] = []
+    PFence: List[CausetEvent] = PStart
+    FFence: List[CausetEvent] = FStart
+    PFenceX: List[Set[CausetEvent]] = []
+    FFenceX: List[Set[CausetEvent]] = []
+    PFenceXIndex: int = 0
+    FFenceXIndex: int = 0
+    isPExtending: bool = (isStartShapeN and isStartCountsEqual) or \
+        (isStartShapeV and (PStartCount < FStartCount))
+    while (PFenceXIndex >= 0) or (FFenceXIndex >= 0):
+        PFenceLen: int = len(PFence)
+        FFenceLen: int = len(FFence)
+        canPTerminate: bool = isStartShapeV and (FFenceLen < PFenceLen)
+        canFTerminate: bool = isStartShapeN and (PFenceLen < FFenceLen)
+        if canPTerminate:
+            pass  # TODO
+        elif canFTerminate:
+            FFenceTerminating: Set[CausetEvent] = FAchain & \
+                PFence[-1].Future & PFence[0].Future
+            for i in range(1, FFenceLen - 1):
+                FFenceTerminating -= PFence[i].Future
+            while FFenceTerminating:
+                e: CausetEvent = FFenceTerminating.pop()
+                FAchain.remove(e)
+                FFenceCopy: List[CausetEvent] = FFence.copy()
+                FFenceCopy.append(e)
+                Fences.append((PFence, FFenceCopy,
+                               -1 if isStartShapeN else 1))
+            FFence.pop(FFenceLen)
+        if canPTerminate or \
+                (isStartShapeN and (PFenceLen == FFenceLen)):
+            pass  # TODO
+        elif canFTerminate or \
+                (isStartShapeV and (FFenceLen == PFenceLen)):
+            if FFenceXIndex > len(FFenceX):
+                FFenceX.append(FAchain & PFence[-1].Future)
+                for i in range(1, FFenceLen - 1):
+                    FFenceX[-1] -= PFence[i].Future
+            if FFenceX[-1]:
+                FFence.append(FFenceX[-1].pop())
+
+        fFenceExtending: Set[CausetEvent]
+    return Fences
+
+
+def __perm_layeredsubset(C: Causet, E: Set[CausetEvent], s: int) -> \
+        Tuple[List[CausetEvent], List[int]]:
+    '''
+    Internal function to find the permutation of a connected 
+    subset `E` of a causet `C`. `s` is the start index of the 
+    permutation.
+
+    This function places the events of the connected subset `E` 
+    that has past and future infinities both with more than one 
+    event. It starts by placing all events of the past and 
+    future infinities and then places the remaining events 
+    element by element.
+    '''
+    # TODO: implement full version
+    # placeholder code:
+    L: List[CausetEvent]  # events sorted by u-coordinates
+    P: List[int]  # permutation for v-coordinates
+    PInf: Set[CausetEvent] = C.PastInfOf(E)
+    FInf: Set[CausetEvent] = C.FutureInfOf(E)
+    # 1. Start with one past infinity event such that the intersection
+    # of its future and the future infinity is maximal.
+    PInf_sorted: List[CausetEvent] = list(PInf)
+    PInf_sorted.sort(key=lambda e: len(e.Future() & FInf), reverse=True)
+    a: CausetEvent = PInf_sorted[0]
+    # 2. Add one future infinity event in the future of event 1 such
+    # that its past intersected with the past infinity is maximal.
+    B: List[CausetEvent] = list(FInf & a.Future())
+    B.sort(key=lambda e: len(e.Past() & PInf), reverse=True)
+    b: CausetEvent = B[0]
+    c: CausetEvent = (b.Past() & PInf - {a}).pop()
+    L = [a, c, b]
+    P = [2, 1, 3]
+
+    return (L, P)
+
+
+def __perm_jointsubset(C: Causet, E: Set[CausetEvent],
+                       s: int) -> Tuple[List[CausetEvent], List[int]]:
+    '''
+    Internal function to find the permutation of a connected 
+    subset `E` of a causet `C`. `s` is the start index of the 
+    permutation.
+
+    This function places the events of the past and future 
+    infinities of `E` if they are single events, respectively, 
+    and repeats the process until both, the past and future 
+    infinities are not singleton anymore.
+    '''
+    L_start: List[CausetEvent] = []  # past events sorted by u-coordinates
+    L_end: List[CausetEvent] = []    # future events sorted by u-coordinates
+    L: List[CausetEvent] = []        # events sorted by u-coordinates
+    P: List[int] = []                # permutation for v-coordinates
+    E_count: int = len(E)
+    Inf: Set[CausetEvent] = C.PastInfOf(E)
+    while len(Inf) == 1:
+        e: CausetEvent = Inf.pop()
+        L_start.append(e)
+        E.remove(e)
+        Inf = C.PastInfOf(E)
+    Inf = C.FutureInfOf(E)
+    while len(Inf) == 1:
+        e: CausetEvent = Inf.pop()
+        L_end.insert(0, e)
+        E.remove(e)
+        Inf = C.FutureInfOf(E)
+    start_len: int = len(L_start)
+    end_len: int = len(L_end)
+    central_subsets = C.disjoint(E)
+    if len(central_subsets) == 1:
+        L, P = __perm_layeredsubset(C, E, s + start_len)
+    else:
+        L, P = __perm_disjointsubsets(C, central_subsets, s + start_len)
+    return (L_start + L + L_end,
+            list(range(s, start_len)) + P +
+            list(range(E_count - end_len + 1, end_len)))
+
+
+def __perm_disjointsubsets(C: Causet, subsets: List[Set[CausetEvent]],
+                           s: int = 1) -> Tuple[List[CausetEvent], List[int]]:
+    '''
+    Internal function to find the permutation of spacelike disjoint 
+    `subsets` of a causet `C`. `s` is the start index of the 
+    permutation (1 by default).
+
+    This function places the disjoint `subsets` from left to right 
+    sorted decreasingly by cardinality.
+    '''
+    L: List[CausetEvent] = []  # events sorted by u-coordinates
+    P: List[int] = []          # permutation for v-coordinates
+    subsets.sort(key=len, reverse=True)
+    subset: Set[CausetEvent]
+    for subset in subsets:
+        subset_L: List[CausetEvent]
+        subset_P: List[int]
+        subset_L, subset_P = __perm_jointsubset(C, subset, s)
+        L = L + subset_L
+        P = subset_P + P
+        s = s + len(subset)
+    return (L, P)
+
+
+def permuted(C: Causet, eventSet: Set[CausetEvent] = None) -> \
+        Tuple[List[CausetEvent], List[int]]:
+    '''
+    Returns the events of `eventSet` (or all events of `C` if `eventSet` 
+    is not specified) in an order such that the order index plus 1  
+    corresponds to the u-coordinate and the permutation (second return 
+    value) corresponds to the v-coordinate when plotted as a Hasse 
+    diagram.
+    '''
+    if eventSet is None:
+        eventSet = C._events
+    return __perm_disjointsubsets(C, C.disjoint(eventSet))
